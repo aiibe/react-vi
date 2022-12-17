f319d7f7663b66abf4f2d49c25ecf27d540f6ed1
Zouk <souk@everblix.fr>
2022-12-18 00:26:04 +0100

    Output tree deps

--- go/go.mod
+++ go/go.mod
@@ -4,4 +4,10 @@ go 1.19
 
 require github.com/samber/lo v1.37.0
 
-require golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+)
--- go/lib/dirt.go
+++ go/lib/dirt.go
@@ -8,35 +8,121 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/fatih/color"
 	"github.com/samber/lo"
 )
 
-func lineHasImport(ext string, line string) bool {
-	if ext == ".tsx" {
+const (
+	TSX = ".tsx"
+)
+
+func cleanString(str string, toReplace []string) string {
+	lo.ForEach(toReplace, func(char string, index int) {
+		str = strings.ReplaceAll(str, char, "")
+	})
+	return str
+}
+
+func getLib(line string, sourcePath string) (string, string) {
+	str := strings.Split(line, " ")
+	length := len(str)
+
+	// Access the element at the last index of the array
+	last := str[length-1]
+
+	// Clean
+	subs := []string{`'`, `"`, `;`}
+	name := cleanString(last, subs)
+
+	// Get filename
+	str2 := strings.Split(name, "/")
+	length2 := len(str2)
+	finalName := str2[length2-1]
+
+	// Source of file
+	sourceFilePath := strings.ReplaceAll(name, "./", sourcePath+"/")
+
+	filePath := filepath.Ext((name))
+	if filePath == "" {
+		sourceFilePath += TSX
+		finalName += TSX
+
+	}
+
+	return finalName, sourceFilePath
+}
+
+func lineHasLib(line string, ext string) bool {
+	if ext == TSX {
 		return strings.HasPrefix(line, "import")
 	}
 	return false
 }
 
-func ReadLines(filePath string) {
-	file, err := os.Open(filePath)
+func hightlight(name string, ext string) string {
+	blue := color.New(color.FgBlue).SprintFunc()
+
+	if filepath.Ext(name) == TSX {
+		return blue(name)
+	}
+
+	return name
+}
+
+func sayFileSize(level int, fullPath string) string {
+	branch := strings.Repeat("-", level-1)
+	fileInfo, _ := os.Stat(fullPath)
+
+	fileName := fileInfo.Name()
+	fileName = hightlight(fileName, TSX)
+
+	if len(branch) == 0 {
+		return fmt.Sprintf("%s (%d B)", fileName, fileInfo.Size())
+	} else {
+		return fmt.Sprintf("%s %s (%d B)", branch, fileName, fileInfo.Size())
+	}
+}
+
+func readFileLines(sourcePath string, fileName string, level int) {
+
+	// Get full path
+	fullPath := sourcePath + "/" + fileName
+
+	// Open file
+	file, err := os.Open(fullPath)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
+	// Announcer
+	say := sayFileSize(level, fullPath)
+	fmt.Println(say)
+
+	// Scan line by line
 	scanner := bufio.NewScanner(file)
 
-	// Filter only lines start with import
 	for scanner.Scan() {
 		line := scanner.Text()
-		isImportLine := lineHasImport(".tsx", line)
+		hasImportLine := lineHasLib(line, TSX)
 
-		if isImportLine {
-			fmt.Println(line)
+		// Filter import lines
+		if hasImportLine {
+			libName, libPath := getLib(line, sourcePath)
+
+			// Show only local lib
+			if strings.HasPrefix(libPath, sourcePath) {
+				if _, err := os.Stat(libPath); os.IsNotExist(err) {
+					// fmt.Println("Not found", libName)
+
+				} else {
+					// fmt.Println("Found", libName, "in", fileName)
+					readFileLines(sourcePath, libName, level+1)
+				}
+			}
 		}
-
 	}
+
 }
 
 func Scan(sourceDir string) {
@@ -57,19 +143,18 @@ func Scan(sourceDir string) {
 		return
 	}
 
-	// Filter .tsx files
-	tsxFiles := lo.Filter(files, func(file fs.FileInfo, index int) bool {
-		return filepath.Ext(file.Name()) == ".tsx"
+	// Get index.tsx
+	entryFiles := lo.Filter(files, func(file fs.FileInfo, index int) bool {
+		return file.Name() == "index.tsx"
 	})
 
-	// Read lines
-	lo.ForEach(tsxFiles, func(file fs.FileInfo, index int) {
+	// Read lines for each entry file
+	lo.ForEach(entryFiles, func(file fs.FileInfo, index int) {
 		fileInfo := fmt.Sprintf("%s - %d Bytes", file.Name(), file.Size())
 		fmt.Println(fileInfo)
 		fmt.Println("-------------------")
 
-		filePath := sourceDir + "/" + file.Name()
-		ReadLines(filePath)
+		readFileLines(sourceDir, file.Name(), 1)
 
 		fmt.Println("")
 	})
