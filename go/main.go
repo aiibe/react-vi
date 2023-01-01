a18f3a62418b0e27ebdaa6197dd88816444a5cc7
souksyp <souk@everblix.fr>
2023-01-01 21:31:02 +0100

    Updated

--- go/main.go
+++ go/main.go
@@ -6,8 +6,8 @@ import (
 	"os"
 	"path/filepath"
 	"souksyp/react-vi/dirt"
+	"souksyp/react-vi/server"
 	"souksyp/react-vi/store"
-	"strings"
 
 	"github.com/ije/esbuild-internal/config"
 	"github.com/ije/esbuild-internal/js_parser"
@@ -25,7 +25,7 @@ func main() {
 	// dirt.OpenBrowser("http://localhost:18881/")
 
 	// Start server
-	// server.Start()
+	server.Start()
 
 }
 
@@ -40,13 +40,25 @@ func buildStaticNodes() {
 	store.NodesMap = dirt.Scan(*root)
 
 	// Scan dependencies for each file
-	firstFilename := "../src/index.tsx"
+	for source, value := range store.NodesMap {
+		if !value.IsDir {
+			readOne(source)
+		}
+	}
 
-	fmt.Println(firstFilename)
+	fmt.Println(store.NodesMap)
+	fmt.Println("---------------------")
+	fmt.Println("Executed", len(store.NodesMap), "files in", time.Since(start))
+}
 
-	data, err := os.ReadFile(firstFilename)
+func readOne(filename string) {
+
+	dirPath, fileName := filepath.Split(filename)
+	fmt.Println("Current filename", dirPath, fileName)
+
+	data, err := os.ReadFile(filename)
 	if err != nil {
-		fmt.Println("Error read file", firstFilename)
+		fmt.Println("Error read file", filename)
 	}
 
 	var logMap dirt.LogMap
@@ -55,13 +67,13 @@ func buildStaticNodes() {
 
 	sourceFile := logger.Source{
 		Index:          0,
-		KeyPath:        logger.Path{Text: firstFilename},
-		PrettyPath:     firstFilename,
+		KeyPath:        logger.Path{Text: filename},
+		PrettyPath:     filename,
 		Contents:       string(data),
-		IdentifierName: firstFilename,
+		IdentifierName: filename,
 	}
 
-	ext := filepath.Ext(firstFilename)
+	ext := filepath.Ext(filename)
 	options := config.Options{Mode: config.ModeBundle}
 	if ext == ".ts" || ext == ".tsx" {
 		options.TS = config.TSOptions{
@@ -77,17 +89,72 @@ func buildStaticNodes() {
 
 	ast, _ := js_parser.Parse(log, sourceFile, js_parser.OptionsFromConfig(&options))
 
-	fmt.Println(len(ast.ImportRecords))
+	fmt.Println("Named imports", len(ast.NamedImports))
 
-	localDependencies := []string{}
-	for _, record := range ast.ImportRecords {
-		if strings.HasPrefix(record.Path.Text, "./") {
-			localDependencies = append(localDependencies, record.Path.Text)
-			fmt.Println(filepath.Join(*root, filepath.Base(record.Path.Text)))
+	// Get named imports
+	var imports []string
+	for _, record := range ast.NamedImports {
+
+		// Current import record
+		importRecord := ast.ImportRecords[record.ImportRecordIndex]
+		importRecordPath := importRecord.Path.Text
+
+		// Alias is default
+		if record.Alias == "default" {
+
+			// Possibly .css, .svg,
+			if filepath.Ext(importRecordPath) == ".css" || filepath.Ext(importRecordPath) == ".svg" {
+				source1 := filepath.Join(dirPath, filepath.Clean(importRecordPath))
+				imports = append(imports, source1)
+				// fmt.Println("ASSETS", (importRecordPath), source1)
+				continue
+			}
+
+			// Handle .component and all....
+			source1 := filepath.Join(dirPath, filepath.Clean(importRecordPath)+".tsx")
+			source2 := filepath.Join(dirPath, filepath.Clean(importRecordPath)+".ts")
+			imports = append(imports, source1)
+			imports = append(imports, source2)
+			// fmt.Println("ASSETS", (importRecordPatht), source1)
+			continue
+		}
+
+		// Has alias .ts or directory with index.ts
+		source1 := filepath.Join(dirPath, filepath.Base(importRecordPath), "index.ts")
+		source2 := filepath.Join(dirPath, filepath.Base(importRecordPath)+".ts")
+		imports = append(imports, source1)
+		imports = append(imports, source2)
+		// fmt.Println("", record.Alias, source1)
+
+	}
+
+	fmt.Println("Named exports star", len(ast.ExportStarImportRecords))
+
+	for _, index := range ast.ExportStarImportRecords {
+		// Current import record
+		importRecord := ast.ImportRecords[index]
+		importRecordPath := importRecord.Path.Text
+
+		source1 := filepath.Join(dirPath, filepath.Base(importRecordPath)+".tsx")
+		source2 := filepath.Join(dirPath, filepath.Base(importRecordPath)+".ts")
+		imports = append(imports, source1)
+		imports = append(imports, source2)
+	}
+
+	fmt.Println("Suggested imports", len(imports), imports)
+
+	var realSources []string
+	for _, source := range imports {
+		_, ok := store.NodesMap[source]
+		if ok {
+			realSources = append(realSources, source)
+			currentFile := store.NodesMap[filename]
+			currentFile.Dependencies = append(currentFile.Dependencies, source)
+			store.NodesMap[filename] = currentFile
 		}
 	}
 
-	fmt.Println(localDependencies)
+	fmt.Println("Imports found", len(realSources), realSources)
 
-	fmt.Println("Executed time", time.Since(start))
+	fmt.Println()
 }
